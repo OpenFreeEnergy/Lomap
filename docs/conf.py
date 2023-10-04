67d0b54b80d227ba99b44e201c37af7b0ff0e289
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-10-04 14:03:05 +1100

    Fix theme

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['ofe_sphinx_theme']
+html_theme = 'ofe_sphinx_theme'
+html_static_path = ['_static']
 html_theme_options = {
     "accent_color": "FeelingBlue",
 }
