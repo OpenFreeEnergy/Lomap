0bd74badeb84b37096fc93754a7668cc9c0c28d4
richard gowers <richardjgowers@gmail.com>
2023-11-02 12:57:40 +0000

    update versioneer for python 3.12

--- lomap/__init__.py
+++ lomap/__init__.py
@@ -74,6 +74,5 @@ del mcs
 
 from . import _due
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
