aaef12a97d8ecb763047db2a7c0f398168834db6
Irfan Alibay <IAlibay@users.noreply.github.com>
2023-11-14 16:07:45 +0000

    Update test_lomapatommapper.py

--- lomap/tests/test_lomapatommapper.py
+++ lomap/tests/test_lomapatommapper.py
@@ -4,8 +4,8 @@ from lomap import LomapAtomMapper
 def test_repr():
     m = LomapAtomMapper()
 
-    assert repr(m) == ("<LomapAtomMapper (time=20, threed=True, max3d=1000.0, "
-                       "element_change=True, seed='', shift=True)>")
+    assert repr(m) == ("<LomapAtomMapper (time=20, threed=True, max3d=1.0, "
+                       "element_change=True, seed='', shift=False)>")
 
     m = LomapAtomMapper(time=15, seed='c1ccccc1')
 
