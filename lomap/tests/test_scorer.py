638130043021ecb22ec2ddb55e7aac659ed946a2
Hannah Baumann <43765638+hannahbaumann@users.noreply.github.com>
2024-05-31 16:11:41 +0200

    Update lomap/tests/test_scorer.py
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>

--- lomap/tests/test_scorer.py
+++ lomap/tests/test_scorer.py
@@ -34,3 +34,23 @@ def test_connected_lomap_network(smcs):
 
     assert network.is_connected() == True
 
+
+def test_ecr_consistency(smcs):
+    lig_4 = [m for m in smcs if m.name == 'lig_4'][0]
+    other_mols = [m for m in smcs if m.name != 'lig_4']
+
+    score_zero = []
+    score_nonzero = []
+    score_dbmol = []
+
+    mapper = lomap.LomapAtomMapper()
+
+    for m in other_mols:
+        mapping = next(mapper.suggest_mappings(lig_4, m))
+        score_zero.append(ecr_score(mapping, charge_changes_score=0.0))
+        score_nonzero.append(ecr_score(mapping, charge_changes_score=0.1))
+        score_dbmol.append(dbmol.ecr(lig_4.to_rdkit(), m.to_rdkit()))
+
+    assert_equal(score_zero, score_dbmol)
+    assert not any(score_zero)
+    assert all([i == 0.1 for i in score_nonzero])
