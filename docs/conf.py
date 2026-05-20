052fa1be8414d55bfadfd5e54b05ef4e6dafa7cd
Irfan Alibay <IAlibay@users.noreply.github.com>
2026-05-20 17:29:27 +0100

    Update CI and increase lowest tested Python version (#124)
    * Raise minimum python version to 3.11 and gufe to 1.0
    * Update CI files

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'ofe_sphinx_theme'
 html_static_path = ['_static']
 html_theme_options = {
-    "accent_color": "FeelingBlue",
+    "accent_color": "cantina-purple",
 }
 html_css_files = []
