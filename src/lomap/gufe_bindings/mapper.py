4d0e5ae10aa7bceb73a5b24214972bdca023d995
IAlibay <IAlibay@users.noreply.github.com>
2026-05-29 08:30:54 +0100

    move the ignore to the right line

--- src/lomap/gufe_bindings/mapper.py
+++ src/lomap/gufe_bindings/mapper.py
@@ -124,8 +124,8 @@ class LomapAtomMapper(AtomMapper):
         # Note: ignoring typing here since there's no way mypy can know
         # that map will return int:int outside of runtime
         mapping_dict: dict[int, int] = dict(
-            map(int, v.split(":"))
-            for v in mapping_string.split(",")  # type: ignore[misc]
+            map(int, v.split(":"))  # type: ignore[misc]
+            for v in mapping_string.split(",")
         )
 
         yield LigandAtomMapping(
